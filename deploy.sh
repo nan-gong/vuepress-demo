e8ecf59d4c5549290cf7bd4901cbff11b16c9fca
Nan Gong <425308731@qq.com>
2020-04-22 12:50:49 +0800

    部署

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果你想要部署到 https://USERNAME.github.io
-git push -f git@github.com:nan-gong/nan-gong.github.io.git master
+sudo git push -f git@github.com:nan-gong/nan-gong.github.io.git master
 
 # 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
 # git push -f git@github.com:USERNAME/<REPO>.git master:gh-pages
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,10 @@
 module.exports = {
     title: '南宫的博客',
+    description: '南宫的博客',
     head: [ // 注入到当前页面的 HTML <head> 中的标签
       ['link', { rel: 'icon', href: '/avatar.png' }], // 增加一个自定义的 favicon(网页标签的图标)
+      ['link', { rel: 'manifest', href: '/avatar.png' }],
+      ['link', { rel: 'apple-touch-icon', href: '/avatar.png' }],
     ],
     markdown: {
         lineNumbers: false // 代码块显示行号
@@ -15,5 +18,6 @@ module.exports = {
       ],
       sidebar: 'auto', // 侧边栏配置
       sidebarDepth: 2, // 侧边栏显示2级
-    }
+    },
+    serviceWorker: true,
   };
